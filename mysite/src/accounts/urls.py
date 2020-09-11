4a6b3fd8b1d4308de5243c809e709c674dafa1ab
BenlarbiNesrine <nininesrin44@gmail.com>
2020-09-11 17:24:55 +0100

    fifth commit

--- mysite/src/accounts/urls.py
+++ mysite/src/accounts/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^logout/$' , LogoutView.as_view() , {'template-name':'registration/logout.html'}),
     url(r'^profile/$' , views.view_profile , name='view_profile'),
     url(r'^profile/edit/$' , views.edit_profile , name='edit_profile'),
-    url(r'^change-password/$' , views.change_password , name='change_password'),
+    url(r'^profile/change-password/$' , views.change_password , name='change_password'),
     url(r'^reset-password/$' , PasswordResetView.as_view() , name='reset_password'),
     url(r'^reset-password/done/$' ,  PasswordResetDoneView.as_view() , name='password_reset_done'),
     url(r'^reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$' ,  PasswordResetConfirmView.as_view() , name='password_reset_confirm'),
--- mysite/src/home/urls.py
+++ mysite/src/home/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url ,include
 from . import views
-from django.contrib.auth.views import LoginView , LogoutView , PasswordResetCompleteView , PasswordResetView , PasswordResetConfirmView ,  PasswordResetDoneView
+
 
 urlpatterns = [
     #url(r'^$', views.home),
